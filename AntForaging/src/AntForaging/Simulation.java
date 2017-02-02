564203d0e72af25449fa717d9b9f1a55b718c462
Xinyu <shguan10@gmail.com>
2017-02-02 15:21:40 -0600

    pseudo code

---
+++ AntForaging/src/AntForaging/Simulation.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AntForaging;
+
+/**
+ *
+ * @author Xinyu
+ */
+public class Simulation {
+
+    double[] fromToStateMatrix;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        Variables:
+        numAnts
+        food sources
+            amount of food at each source
+            amount of phome at each food source
+        transition matrix
+        ant array
+        
+        psuedo-code
+        for each time step
+            for each ant
+                decide next state to go to based on transition matrix
+                update phom and weight for each entry in the transition matrix
+                update where ant is
+            write phome levels to file for pic producing
+        */
+    }
+    
+}
